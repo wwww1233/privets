f8679aaed118abb8000747de4502035a36f24f64
wwww1233 <sororoali00@gmail.com>
2016-07-15 01:52:29 +0430

    Update saveplug.lua

--- plugins/saveplug.lua
+++ plugins/saveplug.lua
@@ -1,6 +1,3 @@
---Created by: @janlou & @Alirezame
---Powered by: @SUDO_TM & @AdvanTM
---⚠️CopyRight all right reserved⚠️
 
 local function saveplug(extra, success, result)
   local msg = extra.msg
@@ -21,19 +18,18 @@ local function run(msg,matches)
     local group = msg.to.id
     if msg.reply_id then
    local name = matches[2]
-      if matches[1] == "save" and matches[2] and is_sudo(msg) then
+      if matches[1] == "سیو" and matches[2] and is_sudo(msg) then
 load_document(msg.reply_id, saveplug, {msg=msg,name=name})
-        return 'Plugin '..name..' has been saved.'
+        return 'پلاگین '..name..' سیو شد '
     end
 end
 end
 return {
   patterns = {
- "^[!/#](save) (.*)$",
+ "(سیو) (.*)$",
   },
   run = run,
 }
 
---Created by: @janlou & @Alirezame
---Powered by: @SUDO_TM & @AdvanTM
---⚠️CopyRight all right reserved⚠️
+--Created by: @shereyder
+--⚠️کپی بدون ذکر منبع حرام است⚠️
